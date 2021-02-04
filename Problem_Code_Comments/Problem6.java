66bae5150626747ea7b281489376096657d64254
Aliana Tejeda <aliana124@gmail.com>
2021-02-04 10:18:19 -0500

    Delete Problem_Code_Comments directory

--- Problem_Code_Comments/Problem6.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Problem6 {
-	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<Integer>(5);
-		int i = list.get(0);
-	}
-}
-//trying to get information from a List which has no data within it.
