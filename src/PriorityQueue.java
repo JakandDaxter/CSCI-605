54ed7277124cbdfe452cbbcea457bfe10f47e238
ReedoMaye <78557054+ReedoMaye@users.noreply.github.com>
2021-03-09 14:09:08 -0600

    PriorityQueue commented

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,3 +1,10 @@
+
+/**
+ * The PriorityQueue interface is of generic type and specifies the behavior of all implementing classes
+ * It extends the Comparable interface and is thus able to determine the priority of objects placed
+ * in the queue in order to insert them correctly
+ */
+
 public interface PriorityQueue<T extends Comparable<T>>{
 
         int maxChild(int index);
