0650518dd09429ee8dc0fce9676bc7fce293f179
ReedoMaye <78557054+ReedoMaye@users.noreply.github.com>
2021-03-09 13:51:15 -0600

    Patron commented

--- src/Patron.java
+++ src/Patron.java
@@ -1,8 +1,22 @@
+
+/**
+ * The Patron class implements the Comparable interface
+ * It stores all patron details and compares patrons based on their coolness factor
+ * Patrons with higher coolness factors are prioritized
+ */
+
 public class Patron implements Comparable<Patron> {
 
     private String name;
     private int coolness;
     private boolean regular;
+
+    /**
+     * Constructor for Patron class
+     * @param coolness
+     * @param regular
+     * @param name
+     */
 
     public Patron(int coolness, boolean regular, String name) {
         this.coolness = coolness;
@@ -10,29 +24,62 @@ public class Patron implements Comparable<Patron> {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
+    /**
+     * Get the Patron name
+     * @return name
+     */
+
+    public String getName() { return name; }
+
+    /**
+     * Set the Patron name
+     * @return name
+     */
 
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the coolness factor
+     * @return coolness
+     */
+
     public int getCoolness() {
         return coolness;
     }
+
+    /**
+     * Set the coolness factor
+     * @return coolness
+     */
 
     public void setCoolness(int coolness) {
         this.coolness = coolness;
     }
 
+    /**
+     * Check Patron regularity
+     * @return regular
+     */
+
     public boolean isRegular() {
         return regular;
     }
 
+    /**
+     * Set Patron regularity
+     * @return regular
+     */
+
     public void setRegular(boolean regular) {
         this.regular = regular;
     }
+
+    /**
+     * Method to compare Patrons to each other based on coolness factor
+     * Patrons with higher coolness factors are prioritized for admittance
+     */
 
     @Override
     public int compareTo(Patron o) {
